c3473b25570166fd80550d9a38e07693efd0d235
James Morris <james.morris@wrenkitchens.com>
2019-11-28 14:32:44 +0000

    All tasks completed. Minor alterations to aesthetic of scroll cue icons and layout on views smaller than native desktop size.

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
-$(document).ready(function() {
-    $("#click").click(function(){
+var rellax = new Rellax('.rellax', {center:true});
+
+
+$(document).ready(function () {
+    $("#click").click(function () {
         $('html, body').animate({
             scrollTop: $("#table").offset().top
         }, 1000);
     });
 });
 
-$(document).ready(function() {
-    $("#landing-arrow").click(function(){
+$(document).ready(function () {
+    $("#landing-arrow").click(function () {
         $('html, body').animate({
             scrollTop: $("#table").offset().top
         }, 1000);
     });
 });
 
-$(document).ready(function() {
-    $("#table-arrow").click(function(){
+$(document).ready(function () {
+    $("#table-arrow").click(function () {
         $('html, body').animate({
             scrollTop: $("#lille").offset().top
         }, 1000);
     });
 });
 
-$(document).ready(function() {
-    $("#main-arrow").click(function(){
+$(document).ready(function () {
+    $("#main-arrow").click(function () {
         $('html, body').animate({
-            scrollTop: $("#research").offset().top
+            scrollTop: $("#footer-arrow").offset().top
         }, 1000);
     });
 });
 
-$(document).ready(function() {
-    $("#footer-arrow").click(function(){
+$(document).ready(function () {
+    $("#footer-arrow").click(function () {
         $('html, body').animate({
             scrollTop: $("#header").offset().top
         }, 1000);
     });
 });
-
-var rellax = new Rellax('.rellax');
